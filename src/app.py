ea7a1064cbe83c33ce1eb3fbfb25ce56331ba0bb
Schwarzart <74467551+algoritm71@users.noreply.github.com>
2026-05-15 10:56:17 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -16,5 +16,5 @@ class SimpleHandler(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     server = HTTPServer(('0.0.0.0', 8000), SimpleHandler)
-    print('asd runningasda on port 8000...')
+    print('asd runnikhjgjkngasda on port 8000...')
     server.serve_forever()
